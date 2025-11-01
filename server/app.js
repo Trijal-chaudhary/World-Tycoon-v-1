6f248f2f1938de80d805cd9e751997c3ea7dcfeb
Trijal-chaudhary <harshvardhanchaudhary86@gmail.com>
2025-11-01 16:35:46 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -137,13 +137,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 
